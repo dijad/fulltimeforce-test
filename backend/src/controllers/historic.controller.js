ab987532276cf9526ea06edd086612a33ec0dda6
dijad <finalcooldown@gmail.com>
2021-08-14 16:43:15 -0500

    Obtención de datos de la API

--- backend/src/controllers/historic.controller.js
+++ backend/src/controllers/historic.controller.js
@@ -2,7 +2,23 @@ const historicCtrl = {};
 const axios = require("axios");
 
 historicCtrl.getCommits = (req, res) => {
-    res.send('HOLA')
+    const url = "https://api.github.com/repos/dijad/fulltimeforce-test/events"
+    axios.get(url)
+        .then(function (response) {
+            const data = response.data;
+            const commits = [];
+            data.forEach(function (element) {
+                commits.push(element.payload.commits)
+            });
+            res.status(200).json({ messazge: 'hola' })
+        })
+        .catch(function (error) {
+            // handle error
+            console.log(error);
+        })
+        .then(function () {
+            // always executed
+        });
 }
 
 module.exports = historicCtrl
